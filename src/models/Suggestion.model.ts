0c6bee81fcb6ff142996f43f3d8a5bc062b45fc1
Ilyat1337 <miningilya2@gmail.com>
2021-06-17 10:57:35 +0300

    refactor: remove unnecessary sensors

--- src/models/Suggestion.model.ts
+++ src/models/Suggestion.model.ts
@@ -5,20 +5,11 @@ export class SuggestionModel extends ObservableObject {
   private static nextId = 1;
   @unobservable public readonly text: string;
   @unobservable public readonly id: number;
-  @unobservable public readonly sensors = new WebSensors();
   public selected = false;
 
   public constructor(text: string) {
     super();
     this.text = text;
     this.id = SuggestionModel.nextId++;
-  }
-
-  @reaction
-  private keyPressed(): void {
-    if (this.sensors.keyboard.down === "Enter") {
-      this.sensors.stopPropagation();
-      this.selected = true;
-    }
   }
 }
